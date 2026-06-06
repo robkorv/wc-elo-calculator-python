f96b8ba7f60d5f3b403c83d0cd19b0e587f4113d
Robbert Korving <r.korving@gmail.com>
2026-06-06 15:04:23 +0200

    use fifa data to map to country

--- cli.py
+++ cli.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 
+import json
 import pathlib
 import requests
 import logging
 import csv
 import argparse
-import json
 
 file_path = pathlib.Path(__file__)
 working_dir = file_path.parent
@@ -14,20 +14,11 @@ logger = logging.getLogger(file_path.stem)
 
 
 elo_per_team = {}
-alpha_3_to_2 = {}
-alpha_3_to_name = {}
+fifa_code_to_alpha_2 = {}
+fifa_code_to_name = {}
 
 
-def main(args):
-    iso_3166 = json.loads(
-        pathlib.Path("/usr/share/iso-codes/json/iso_3166-1.json").read_text()
-    )
-    for item in iso_3166["3166-1"]:
-        alpha_3_to_2[item["alpha_3"]] = item["alpha_2"]
-        alpha_3_to_name[item["alpha_3"]] = item["name"]
-    alpha_3_to_2["SCT"] = "SQ"
-    alpha_3_to_name["SCT"] = "Scotland"
-    logger.info(f"{working_dir = }")
+def load_elo_per_team():
     elo_tsv = working_dir.joinpath("2026_World_Cup.tsv")
     if not elo_tsv.is_file():
         logger.info(f"Downloading Elo ratings file: {elo_tsv}")
@@ -38,6 +29,52 @@ def main(args):
         reader = csv.reader(f, dialect="excel-tab")
         for row in reader:
             elo_per_team[row[2]] = int(row[3])
+    logger.info(f"Elo ratings loaded from file: {elo_tsv}")
+
+
+def load_fifa_code_to_alpha_2():
+    fifa_member_associations = working_dir.joinpath("fifa-member-associations.csv")
+    if not fifa_member_associations.is_file():
+        logger.info(
+            f"Downloading FIFA member associations file: {fifa_member_associations}"
+        )
+        with requests.get(
+            "https://raw.githubusercontent.com/openpotato/fifa-codes/refs/heads/main/src/fifa-member-associations.csv"
+        ) as r:
+            r.raise_for_status()
+            fifa_member_associations.write_bytes(r.content)
+    with fifa_member_associations.open() as f:
+        reader = csv.DictReader(f, dialect="excel")
+        for row in filter(lambda x: x["Country.Iso3166.Alpha2Code"] in fifa_code_to_alpha_2, reader):
+            fifa_code_to_alpha_2[row["FIFA.Code"]] = row["Country.Iso3166.Alpha2Code"]
+    logger.info(
+        f"FIFA member associations loaded from file: {fifa_member_associations}"
+    )
+
+
+def load_fifa_code_to_name():
+    iso_3166 = pathlib.Path("/usr/share/iso-codes/json/iso_3166-1.json")
+    logger.info(f"Loading ISO 3166-1 codes from file: {iso_3166}")
+    iso_3166_data = json.loads(
+        pathlib.Path("/usr/share/iso-codes/json/iso_3166-1.json").read_text()
+    )
+    alpha_2_to_name = {}
+    for item in filter(
+        lambda x: x["alpha_2"] in fifa_code_to_alpha_2.values(), iso_3166_data["3166-1"]
+    ):
+        alpha_2_to_name[item["alpha_2"]] = item["name"]
+
+    for fifa_code, alpha_2 in fifa_code_to_alpha_2.items():
+        fifa_code_to_name[fifa_code] = alpha_2_to_name[alpha_2]
+    logger.info("FIFA code to name mapping loaded from ISO 3166-1 data")
+
+
+def main(args):
+    logger.info(f"{working_dir = }")
+
+    load_fifa_code_to_alpha_2()
+    load_elo_per_team()
+    load_fifa_code_to_name()
 
     highest_elo = max(elo_per_team.values())
     logger.info(f"Highest Elo rating: {highest_elo}")
@@ -46,16 +83,17 @@ def main(args):
     max_diff = highest_elo - lowest_elo
     logger.info(f"Max Elo rating difference: {max_diff}")
     avg_goals = 2.69
+    logger.info(f"Average goals: {avg_goals}")
 
     def calculate_win_probability(country1, country2):
-        diff1 = elo_per_team[alpha_3_to_2[country1]] - lowest_elo
-        diff2 = elo_per_team[alpha_3_to_2[country2]] - lowest_elo
+        diff1 = elo_per_team[fifa_code_to_alpha_2[country1]] - lowest_elo
+        diff2 = elo_per_team[fifa_code_to_alpha_2[country2]] - lowest_elo
         cupwinratio1 = diff1 / max_diff
         cupwinratio2 = diff2 / max_diff
         goals1 = round(cupwinratio1 / (cupwinratio1 + cupwinratio2) * avg_goals)
         goals2 = round(cupwinratio2 / (cupwinratio1 + cupwinratio2) * avg_goals)
-        country1 = alpha_3_to_name[country1]
-        country2 = alpha_3_to_name[country2]
+        country1 = fifa_code_to_name[country1]
+        country2 = fifa_code_to_name[country2]
         return {
             "country1": country1,
             "country2": country2,
